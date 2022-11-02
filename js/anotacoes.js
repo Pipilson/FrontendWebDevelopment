27a4ac466ce75a2d4c72b3c513162b8eb0c8b1ff
Pipilson <pietro.varraschim@hotmail.com>
2022-11-01 22:57:44 -0300

    Fim Aula 4

--- js/anotacoes.js
+++ js/anotacoes.js
@@ -28,3 +28,49 @@ minhaFuncao(); //Retorna 20
 //Usar CONST para variaveis que não vão mudar
 //Usar LET para variaveis que vão mudar
 //Evitar usar VAR
+
+const a = 4;
+const b = 6;
+
+//Operadores
+a + b; //Soma
+a - b; //Subtração
+a * b; //Multiplicação
+a / b; //Divisão
+a % b; //Módulo
+a ** b; //Exponenciação
+a++; //a = a + 1
+a--; //a = a - 1
+
+a = b; //a = 6
+a += b; //a = a + b
+a -= b; //a = a - b
+a *= b; //a = a * b
+a /= b; //a = a / b
+
+a == b; //igual em valor
+a === b; //igual em valor e tipo
+a != b; //diferente
+a !== b; //diferente em valor e tipo
+a > b; //a maior que b
+a < b; //a menor que b
+a >= b; //a maior ou igual que b
+a <= b; //a menor ou igual que b
+
+//Bizarrice do JavaScript
+const Array1 = [1, 2, 3, 4, 5];
+const Array2 = [1, 2, 3, 4, 5];
+
+Array1 === Array1 //true
+Array1 == Array2 //false
+Array1 === Array2 //false -- O mesmo acontece com Objetos
+//Pesquisar como comparar Arrays (Lodash)
+
+let resultado1 = 20 === 20 ? 'values match' : "values do not match"; //ternário
+//Significa:
+let resultado2;
+if (20 === 20) {
+    resultado2 = 'values match';
+} else {
+    resultado2 = 'values do not match';
+}
